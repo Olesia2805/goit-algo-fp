62ec06ed63fa79d964469fdb0fcf762c1edbf5bf
Olesia2805 <vysotina2002@gmail.com>
2024-03-21 17:24:14 +0100

    pifagor_tree

--- pifagor_tree.py
+++ pifagor_tree.py
@@ -8,21 +8,21 @@ def draw_pifagor_tree(depth, size):
     
     turtle.forward(size)
     turtle.left(45)
-    draw_pifagor_tree(depth-1, size/math.sqrt(2))
+    draw_pifagor_tree(depth - 1, size / math.sqrt(2))
     turtle.right(90)
-    draw_pifagor_tree(depth-1, size/math.sqrt(2))
+    draw_pifagor_tree(depth - 1, size / math.sqrt(2))
     turtle.left(45)
     turtle.backward(size)
 
 def main():
-    
+
     turtle.speed(0)
     turtle.left(90)
     turtle.penup()
     turtle.goto(0, -300)
     turtle.pendown()
     turtle.color("green")
-    draw_pifagor_tree(5, 200)
+    draw_pifagor_tree(8, 200)
     turtle.done()
 
 if __name__ == "__main__":
