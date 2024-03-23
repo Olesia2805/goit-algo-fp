ba07409704e1df9bdcd63ee5ab10bf6f725f3fa4
Olesia2805 <vysotina2002@gmail.com>
2024-03-23 23:08:25 +0100

    dynamic_and_greedy_algorithm

--- DataStructures.py
+++ DataStructures.py
@@ -1,7 +1,176 @@
-'''Завдання 1. Структури даних. Сортування. Робота з однозв'язним списком
+import sortedcontainers
 
-Для реалізації однозв'язного списку (приклад реалізації можна взяти з конспекту) необхідно:
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-написати функцію, яка реалізує реверсування однозв'язного списку, змінюючи посилання між вузлами;
-розробити алгоритм сортування для однозв'язного списку, наприклад, сортування вставками або злиттям;
-написати функцію, що об'єднує два відсортовані однозв'язні списки в один відсортований список.'''
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+
+    def insert_after(self, prev_node: Node, data):
+        if prev_node is None:
+            print("Попереднього вузла не існує.")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key: int):
+        cur = self.head
+        if cur and cur.data == key:
+            self.head = cur.next
+            cur = None
+            return
+        prev = None
+        while cur and cur.data != key:
+            prev = cur
+            cur = cur.next
+        if cur is None:
+            return
+        prev.next = cur.next
+        cur = None
+
+    def search_element(self, data: int) -> Node | None:
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                return cur
+            cur = cur.next
+        return None
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, "-->", end="")
+            current = current.next
+        print('None')
+
+    def reverse(self):
+        prev_node = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head = prev_node
+
+    def merge_sort(self, head):
+
+        if head is None or head.next is None:
+            return head
+
+        mid = self.get_middle(head)
+        left_half = head
+        right_half = mid.next
+        mid.next = None
+
+        left = self.merge_sort(left_half)
+        right = self.merge_sort(right_half)
+
+        sorted_list = self.merge(left, right)
+        return sorted_list
+
+    def get_middle(self, head):
+        if head is None:
+            return head
+
+        slow_ptr = head
+        fast_ptr = head
+        while fast_ptr.next and fast_ptr.next.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+        return slow_ptr
+
+    def merge(self, a, b):
+        result = None
+
+        if a is None:
+            return b
+        if b is None:
+            return a
+
+        if a.data <= b.data:
+            result = a
+            result.next = self.merge(a.next, b)
+        else:
+            result = b
+            result.next = self.merge(a, b.next)
+
+        return result
+
+'''    def merge_sorted_lists(self, list1, list2):
+        dummy_head = Node(0)
+        tail = dummy_head
+        l1 = list1.head  # Отримуємо голову списку
+        l2 = list2.head  # Отримуємо голову списку
+
+        while True:
+            if l1 is None:
+                tail.next = l2
+                break
+            if l2 is None:
+                tail.next = l1
+                break
+
+            if l1.data <= l2.data:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+
+            tail = tail.next
+        
+        merged_head = dummy_head.next
+
+        sorted_list = self.merge_sort(merged_head)
+
+        return sorted_list'''
+
+if __name__ == '__main__':
+
+    first_list = LinkedList()
+
+    first_list.insert_at_beginning(5)
+    first_list.insert_at_beginning(10)
+    first_list.insert_at_beginning(15)
+    first_list.insert_at_end(20)
+    first_list.insert_at_end(25)
+    print("Зв'язний список:")
+    first_list.print_list()
+
+    first_list.reverse()
+    print("Зв'язний список після реверсування :")
+    first_list.print_list()
+
+    first_list.head = first_list.merge_sort(first_list.head)
+    print("Зв'язний список відсортовано:")
+    first_list.print_list()
+
+    second_list = LinkedList()
+    first_list.insert_at_beginning(59)
+    first_list.insert_at_beginning(20)
+    first_list.insert_at_beginning(35)
+
+    first_list.merge_sorted_lists(first_list, second_list)
+    print("Зв'язний список відсортовано та замерджено:")
+    first_list.print_list()
--- dynamic_and_greedy_algorithm.py
+++ dynamic_and_greedy_algorithm.py
@@ -1,8 +1,3 @@
-'''Завдання 6: Жадібні алгоритми та динамічне програмування
-
-Необхідно написати програму на Python, яка використовує два підходи — жадібний алгоритм та алгоритм динамічного програмування для розв’язання задачі вибору їжі з найбільшою сумарною калорійністю в межах обмеженого бюджету.
-
-Кожен вид їжі має вказану вартість і калорійність. Дані про їжу представлені у вигляді словника, де ключ — назва страви, а значення — це словник з вартістю та калорійністю.
 
 items = {
     "pizza": {"cost": 50, "calories": 300},
@@ -13,6 +8,60 @@ items = {
     "potato": {"cost": 25, "calories": 350}
 }
 
-Розробіть функцію greedy_algorithm жадібного алгоритму, яка вибирає страви, максимізуючи співвідношення калорій до вартості, не перевищуючи заданий бюджет.
 
-Для реалізації алгоритму динамічного програмування створіть функцію dynamic_programming, яка обчислює оптимальний набір страв для максимізації калорійності при заданому бюджеті.'''
+def greedy_algorithm(items, budget):
+    total_calories = 0
+    remaining_budget = budget
+    chosen_items = []
+
+    sorted_items = dict(sorted(items.items(), key=lambda item: item[1]['calories'], reverse=True))
+
+    for item, value in sorted_items.items():
+        if remaining_budget >= value['cost']:
+            chosen_items.append(item)
+            total_calories += value['calories']
+            remaining_budget -= value['cost']
+
+    return total_calories, budget - remaining_budget, chosen_items
+
+
+def dynamic_programming(items, budget):
+    item_names = list(items.keys())
+    num_items = len(item_names)
+
+    dp_table = [[0 for _ in range(budget + 1)] for _ in range(num_items + 1)]
+
+    for i in range(1, num_items + 1):
+        for w in range(1, budget + 1):
+            item_cost = items[item_names[i - 1]]['cost']
+            item_calories = items[item_names[i - 1]]['calories']
+            if item_cost <= w:
+                dp_table[i][w] = max(dp_table[i - 1][w], dp_table[i - 1][w - item_cost] + item_calories)
+            else:
+                dp_table[i][w] = dp_table[i - 1][w]
+
+    chosen_items = []
+    temp_calories = dp_table[num_items][budget]
+    remaining_budget = budget
+    i = num_items
+    w = budget
+
+    while i > 0 and temp_calories > 0:
+        if temp_calories != dp_table[i - 1][w]:
+            chosen_items.append(item_names[i - 1])
+            temp_calories -= items[item_names[i - 1]]['calories']
+            remaining_budget -= items[item_names[i - 1]]['cost']
+            w -= items[item_names[i - 1]]['cost']
+        i -= 1
+
+
+    return dp_table[num_items][budget], budget - remaining_budget, chosen_items
+
+if __name__ == '__main__':
+
+    budget = 100
+
+    greedy_result = greedy_algorithm(items, budget)
+    dp_result = dynamic_programming(items, budget)
+
+    print(greedy_result, dp_result)
